8a924f7e83f418885542bd8d9c062a5b9cc87ac6
unknown <627915@CHNB101W09.lr.k12.nj.us>
2014-06-11 11:11:33 -0400

    Made the base game actually work in eclipse

---
+++ src/Base.java
@@ -0,0 +1,34 @@
+import java.applet.Applet;
+
+
+public class Base extends Applet implements Runnable{
+
+	Thread t;
+	String _message;
+	
+	public void init(){
+		t = new Thread(this);
+		t.start();
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		while(true){
+			repaint();
+			try{
+                t.sleep(1000/30);
+            }   catch (InterruptedException e) { ; }
+		}
+	}
+	
+	public boolean keyUp(Event e, int key){
+		_message = "Value: " + key;
+		return true;
+	}
+	
+	public void paint(Graphics g){
+		g.drawString(_message, 10, 20);
+	}
+
+}
