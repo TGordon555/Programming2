bbf86891e655e0fc8d896ac58b795ed41a48dfe1
unknown <627915@CHNB101W09.lr.k12.nj.us>
2014-06-11 10:56:50 -0400

    Started on the game

--- src/Game.java
+++ src/Game.java
@@ -3,10 +3,23 @@ import java.applet.Applet;
 
 public class Game extends Applet implements Runnable{
 
+	Thread t;
+	
+	public void init(){
+		t = new Thread(this);
+		t.start();
+	}
+	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		//this is a test, please ignore
+		while(true){
+			repaint();
+			try{
+                t.sleep(1000/30);
+            }   catch (InterruptedException e) { ; }
+		}
 	}
 
 }
