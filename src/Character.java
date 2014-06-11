7e1f8101b692d072d87c9ed903ef061c679055da
unknown <669514@CHNB101W10.lr.k12.nj.us>
2014-06-11 11:35:25 -0400

    STOP TELLING ME WHAT TO DO

--- src/Character.java
+++ src/Character.java
@@ -1,3 +1,9 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 
 public class Character {
 	
@@ -7,10 +13,17 @@ public class Character {
 	 */
 	int _height; 
 	Location _characterLoc;
+	BufferedImage sprite;
 	
 	public Character(int height, Location loc){
 		_height = height;
 		_characterLoc = loc;
+		try {
+			BufferedImage sprite = ImageIO.read(new File( "character.png" ));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public int getHeight(){
@@ -25,5 +38,8 @@ public class Character {
 		_characterLoc = newLoc;
 	}
 	
+	public BufferedImage getSprite(){
+		return sprite;
+	}
 	
 }
